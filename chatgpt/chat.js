87bdfe8da600385a5aa17517f5f9ec6c393101bc
Monalisa-Jena <154300434+Monalisa-XD@users.noreply.github.com>
2024-02-10 20:22:51 +0530

    Change API key

--- chatgpt/chat.js
+++ chatgpt/chat.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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";
+const API_KEY = "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";
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
